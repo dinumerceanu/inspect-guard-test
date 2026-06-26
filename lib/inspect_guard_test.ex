ef4b553f2d8834b3162f71b91ef8687e6b40035e
dinumerceanu <dinu.eu@yahoo.com>
2026-06-26 11:51:11 +0300

    initial commit

---
+++ lib/inspect_guard_test.ex
@@ -0,0 +1,18 @@
+defmodule InspectGuardTest do
+  @moduledoc """
+  Documentation for `InspectGuardTest`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> InspectGuardTest.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/inspect_guard_test_test.exs
@@ -0,0 +1,8 @@
+defmodule InspectGuardTestTest do
+  use ExUnit.Case
+  doctest InspectGuardTest
+
+  test "greets the world" do
+    assert InspectGuardTest.hello() == :world
+  end
+end
